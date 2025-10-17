1235a07140838d2b2da6a45e55ba132c6dd58ab6
MatheusBlanco <msallesblanco@gmail.com>
2025-10-17 11:46:55 -0300

    fix: :bug: Adds progress bar

--- src/components/ui/site-header.tsx
+++ src/components/ui/site-header.tsx
@@ -2,10 +2,14 @@ import { Separator } from "@/components/ui/separator";
 import { SidebarTrigger } from "@/components/ui/sidebar";
 import { useMonthlyCredits } from "@/features/Credits/hooks/useCredits";
 import { Sparkles } from "lucide-react";
+import { Progress } from "./progress";
 
 export function SiteHeader({ title }: { title: string }) {
   const { data: userCredits } = useMonthlyCredits();
   const balance = Number(userCredits?.monthly_status?.monthly_remaining) || 0;
+  const allocation =
+    Number(userCredits?.subscription_info?.monthly_credits_allocation) || 0;
+  const percentage = allocation > 0 ? (balance / allocation) * 100 : 0;
 
   return (
     <header className="flex h-(--header-height) shrink-0 items-center gap-2  transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
@@ -18,8 +22,9 @@ export function SiteHeader({ title }: { title: string }) {
           <div className="flex items-center gap-4">
             <div className="flex items-center gap-2">
               <Sparkles className="h-4 w-4 text-primary" />
+              {percentage.toFixed(2)}%
               <span className="w-[120px] text-muted-foreground text-sm">
-                R${balance} restantes
+                <Progress value={percentage} />
               </span>
             </div>
           </div>
